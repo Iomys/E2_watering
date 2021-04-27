805ac896eff6bd35b71f0f165b8faa8f701c4bc1
Tchavo <tchavo.frund@students.hevs.ch>
2021-04-27 16:07:27 +0200

    Calcul de la température '

--- start.py
+++ start.py
@@ -1,12 +1,32 @@
+# Import the needed modules
+import grovepi
 import time
 
-variable = 1+100
-print(variable)
+# Connect the LED to digital port D4
+LED = 4
 
-variable
+# Digital port D4 set to OUTPUT
+grovepi.pinMode(led,"OUTPUT")
 
-variable2 = variable + 20
+# Initial delay of 1 second
+time.sleep(1)
 
-time.sleep(100)
-
+while True:
+    try:
+        # Blink the LED
+        # Send HIGH to switch on LED
+        grovepi.digitalWrite(led,1)
+        time.sleep(1)
+        # Send LOW to switch off LED
+        grovepi.digitalWrite(led,0)
+        time.sleep(1)
+    except KeyboardInterrupt as e:
+        # Turn the LED off before stopping
+        print(str(e))
+        grovepi.digitalWrite(led,0)
+        break
+    except (IOError, TypeError) as e:
+        # Turn the LED off before stopping
+        print(str(e))
+        grovepi.digitalWrite(led,0)
 
