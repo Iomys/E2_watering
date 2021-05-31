cd9c5ab03ce82be6d0ab5961329a2f859790e05a
iomys <valentin.sandoz@vieux-mazel.ch>
2021-05-31 22:48:52 +0200

    Modification du fichier de mesure en cronjob

--- soil_cron.py
+++ soil_cron.py
@@ -5,7 +5,7 @@ mes = Measures()
 print("Iinit")
 with open('/home/pi/E2_watering/report/soil_moisture.csv', 'a', newline='') as csvfile:
 
-    output = [datetime.now().isoformat(), mes.soil_hum_chin(), mes.soil_hum_cap1(), mes.soil_hum_cap2(), mes.ds18b20.get_temperature(), mes.aht10_temp(), mes.aht10_hum()]
+    output = [datetime.now().isoformat(), "" , mes.soil_hum_cap1(), mes.soil_hum_cap2(), mes.ds18b20.get_temperature(), mes.aht10_temp(), mes.aht10_hum(), mes.soil_hum_cap0()]
     writer = csv.writer(csvfile, delimiter=',',
                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(output)
